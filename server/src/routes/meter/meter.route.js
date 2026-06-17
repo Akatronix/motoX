7008249e8b473eafd4ea7eca654cd16ead48d8c2
Akatronix <153951875+Akatronix@users.noreply.github.com>
2026-06-17 01:49:35 +0100

    Add routes for updating and getting max parameters

--- server/src/routes/meter/meter.route.js
+++ server/src/routes/meter/meter.route.js
@@ -5,12 +5,20 @@ const {
 const {
   hardwareData,
 } = require("../../controllers/meter/hardwareData.controller");
+const {
+  updateMaxParam,
+} = require("../../controllers/meter/updateMaxParam.controller");
+const {
+  getMaxParams,
+} = require("../../controllers/meter/getMaxParams.controller");
 
 const { verifyUser } = require("../../middlewares/VerifyUser");
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createMotorData);
+router.post("/max-params", verifyUser, updateMaxParam);
+router.get("/max-params", verifyUser, getMaxParams);
 router.post("/hardware", hardwareData);
 
 module.exports = router;
